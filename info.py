29e0e1900d0aab8e018115a5301a1333476fc8e2
Oktay Acikalin <ok@ryotic.de>
2009-02-26 18:22:10 +0100

    info.py vervollständigt

--- info.py
+++ info.py
@@ -2,44 +2,44 @@
 
 # package info
 name = 'symmetrics_module_invoicepdf'
-tags = ()
+tags = ('magento', 'module', 'symmetrics')
 
 # relations
 requires = {
+    'magento': '*',
+    'symmetrics_config_german': '*',
+    'symmetrics_module_impressum': '*',
 }
 excludes = {
 }
 
 # who is responsible for this package?
 team_leader = {
-    'Leader': 'info@symmetrics.de'
+    'Sergej Braznikov': 'sb@symmetrics.de'
 }
 
 # who should check this package in the first place?
 maintainer = {
-    'Developer': 'info@symmetrics.de'
+    'Eugen Gitin': 'eg@symmetrics.de'
 }
 
 # relative installation path (e.g. app/code/local)
 install_path = ''
 
 # additional infos
-info = '[put some info here]'
+info = 'symmetrics Rechnungsvorlage'
 summary = '''
-    [put more info here.
-    blaaa blaa
-    blaa blaaaaa
-    blaaaa.]
+    Rechtssichere (Deutschland) Vorlage für die Rechnungen
 '''
 license = 'AFL 3.0'
 authors = {
-    'John Doe': 'info@symmetrics.de'
+    'Eugen Gitin': 'eg@symmetrics.de'
 }
 homepage = 'http://www.symmetrics.de'
 
 # files this package depends on
 depends_on_files = (
-    'app/code/core/.../1.php',
-    'app/code/core/.../2.php',
-    'app/code/core/.../3.php',
+    'app/code/core/Mage/Core/Helper/Abstract.php',
+    'app/code/core/Mage/Sales/Model/Order/Pdf/Items/Invoice/Default.php',
+    'app/code/core/Mage/Sales/Model/Order/Pdf/Abstract.php',
 )
