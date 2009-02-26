6b4c07eb4fc9a18c4d390792146843889e4e2757
saasrepo <saasrepo@symdev.symmetrics.de>
2009-02-26 14:33:45 +0100

    Initial import

---
+++ info.py
@@ -0,0 +1,45 @@
+# -- encoding: utf-8 --
+
+# package info
+name = 'symmetrics_module_invoicepdf'
+tags = ()
+
+# relations
+requires = {
+}
+excludes = {
+}
+
+# who is responsible for this package?
+team_leader = {
+    'Leader': 'info@symmetrics.de'
+}
+
+# who should check this package in the first place?
+maintainer = {
+    'Developer': 'info@symmetrics.de'
+}
+
+# relative installation path (e.g. app/code/local)
+install_path = ''
+
+# additional infos
+info = '[put some info here]'
+summary = '''
+    [put more info here.
+    blaaa blaa
+    blaa blaaaaa
+    blaaaa.]
+'''
+license = 'AFL 3.0'
+authors = {
+    'John Doe': 'info@symmetrics.de'
+}
+homepage = 'http://www.symmetrics.de'
+
+# files this package depends on
+depends_on_files = (
+    'app/code/core/.../1.php',
+    'app/code/core/.../2.php',
+    'app/code/core/.../3.php',
+)
