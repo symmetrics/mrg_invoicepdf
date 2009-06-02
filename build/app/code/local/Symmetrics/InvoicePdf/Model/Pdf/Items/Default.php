9537edd772337d2c43fe6aa8aafad04039e0d474
Eugen Gitin <devnull@localhost>
2009-06-02 13:59:50 +0200

    Item-Rendering nach neuen Vorgaben umgebaut

--- build/app/code/local/Symmetrics/InvoicePdf/Model/Pdf/Items/Default.php
+++ build/app/code/local/Symmetrics/InvoicePdf/Model/Pdf/Items/Default.php
@@ -9,100 +9,102 @@
  * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Symmetrics_InvoicePdf_Model_Pdf_Items_Default extends Mage_Sales_Model_Order_Pdf_Items_Invoice_Default
-{	
+{
     public function draw($position = 1)
     {
-        $order = $this->getOrder();
-        $item = $this->getItem();
-        $pdf = $this->getPdf();
-        $page = $this->getPage();
-        $shift = array(0, 10, 0);
+        $order  = $this->getOrder();
+        $item   = $this->getItem();
+        $pdf    = $this->getPdf();
+        $page   = $this->getPage();
+        $lines  = array();
+        
+        $fontSize = 9;
 
-        $font = $this->_setFontRegular();
+        // draw Position Number
+        $lines[0]= array(array(
+            'text'  => $position,
+            'feed'  => $pdf->margin['left'] + 20,
+            'align' => 'right',
+            'font_size' => $fontSize
+        ));
+        
+        // draw SKU
+        $lines[0][] = array(
+            'text'  => Mage::helper('core/string')->str_split($this->getSku($item), 10),
+            'feed'  => $pdf->margin['left'] + 45,
+            'font_size' => $fontSize
+        );
+        
+        // draw Product name
+        $lines[0][]= array(
+            'text' => Mage::helper('core/string')->str_split($item->getName(), 60, true, true),
+            'feed' => $pdf->margin['left'] + 110,
+            'font_size' => $fontSize
+        );
 
-        /* position number */
-        $page->drawText($position, $pdf->margin['left'] + 20 - $pdf->widthForStringUsingFontSize($position, $font, 9), $pdf->y, $pdf->encoding);
+        // draw QTY
+        $lines[0][] = array(
+            'text'  => $item->getQty() * 1,
+            'feed'  => $pdf->margin['right'] - 110,
+            'align' => 'right',
+            'font_size' => $fontSize
+        );
 
-        /* article name, split after 35 chars */
-		foreach(Mage::helper('core/string')->str_split($item->getName(), 35, true, true) as $key => $part) {
-			$page->drawText($part, $pdf->margin['left'] + 110, $pdf->y - $shift[0], $pdf->encoding);
-			$shift[0] += 10;
-        }
+        if ($options = $this->getItemOptions()) 
+        {
+            foreach ($options as $option) 
+            {
+                // draw options label
+                $lines[][] = array(
+                    'text' => Mage::helper('core/string')->str_split(strip_tags($option['label']), 40, false, true),
+                    'font' => 'regular',
+                    'feed' => $pdf->margin['left']
+                );
 
-        $options = $this->getItemOptions();
-
-        if (isset($options)) {
-            foreach ($options as $option) {
-                foreach(Mage::helper('core/string')->str_split(strip_tags($option['label']), 40, false, true) as $_option) {
-                	$page->drawText($_option, $pdf->margin['left'] + 110, $pdf->y - $shift[0], $pdf->encoding);
-                    $shift[0] += 10;
-                }
-
+                // draw options value
                 if ($option['value']) {
-
                     $_printValue = isset($option['print_value']) ? $option['print_value'] : strip_tags($option['value']);
                     $values = explode(', ', $_printValue);
-
                     foreach ($values as $value) {
-                        foreach (Mage::helper('core/string')->str_split($value, 60, true, true) as $_value) {
-                            $page->drawText($_value, $pdf->margin['left'] + 120, $pdf->y - $shift[0], $pdf->encoding);
-                            $shift[0] += 10;
-                        }
+                        $lines[][] = array(
+                            'text' => Mage::helper('core/string')->str_split($value, 60, true, true),
+                            'feed' => $pdf->margin['left'] + 120
+                        );
                     }
                 }
             }
         }
 
-        foreach ($this->_parseDescription() as $description) {
-            $page->drawText(strip_tags($description), 65, $pdf->y - $shift[1], $pdf->encoding);
-            $shift[1] += 10;
-        }
+        // draw Price
+        $lines[0][] = array(
+            'text'  => $order->formatPriceTxt($item->getPrice()),
+            'feed'  => $pdf->margin['right'] - 160,
+            'align' => 'right',
+            'font_size' => $fontSize
+        );
 
-        /* sku */       
-        foreach (Mage::helper('core/string')->str_split($this->getSku($item), 10) as $key => $part) {
-			if ($key > 0) {
-				$shift[2] += 10;
-            }
-			$page->drawText($part, $pdf->margin['left'] + 45, $pdf->y-$shift[2], $pdf->encoding);
-        }
+        // draw Tax
+        $lines[0][] = array(
+            'text'  => $order->formatPriceTxt($item->getTaxAmount()),
+            'feed'  => $pdf->margin['right'] - 65,
+            'align' => 'right',
+            'font_size' => $fontSize
+        );
 
-        /* single price */
-        $price = $order->formatPriceTxt($item->getPrice());
-        $page->drawText($price, $pdf->margin['right'] - 160 - $pdf->widthForStringUsingFontSize($price, $font, 9), $pdf->y, $pdf->encoding);
-        
-        /* quantity */
-        $quantity = $item->getQty() * 1;
-        $page->drawText($quantity, $pdf->margin['right'] - 110 - $pdf->widthForStringUsingFontSize($quantity, $font, 9), $pdf->y, $pdf->encoding);
-        
-        /* tax amount */
-        $tax = $order->formatPriceTxt($item->getTaxAmount());
-        $page->drawText($tax, $pdf->margin['right'] - 65 - $pdf->widthForStringUsingFontSize($tax, $font, 9), $pdf->y, $pdf->encoding);
-        
-        /* row total */
-        $row_total = $order->formatPriceTxt($item->getRowTotal() + $item->getTaxAmount());
-        $page->drawText($row_total, $pdf->margin['right'] - 10 - $pdf->widthForStringUsingFontSize($row_total, $font, 9), $pdf->y, $pdf->encoding);
-        
-        $pdf->y -= max($shift) + 10;
-    }
-    
-    protected function _setFontRegular($size = 10)
-    {
-    	$font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA);
-        $this->setFont($font, $size);
-        return $font;
-    }
+        // draw Subtotal
+        $lines[0][] = array(
+            'text'  => $order->formatPriceTxt($item->getRowTotal()),
+            'feed'  => $pdf->margin['right'] - 10,
+            'align' => 'right',
+            'font_size' => $fontSize
+        );
 
-    protected function _setFontBold($size = 10)
-    {
-        $font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_BOLD);
-        $this->setFont($font, $size);
-        return $font;
-    }
+        $lineBlock = array(
+            'lines'  => $lines,
+            'height' => 15
+        );
 
-    protected function __setFontItalic($size = 10)
-    {
-        $font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_ITALIC);
-        $this->setFont($font, $size);
-        return $font;
+        $page = $pdf->drawLineBlocks($page, array($lineBlock), array('table_header' => true));
+        $this->setPage($page);
     }
 }
