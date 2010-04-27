0a43d78de238c6e34043f4c2e30f9c2d619bcd71
Torsten Walluhn <tw@symmetrics.de>
2010-04-27 12:07:33 +0200

    Symmetrics_InvoicePdf_Model_Pdf_Items_Totals komplett mit PHPDoc dokumentiert

--- build/app/code/community/Symmetrics/InvoicePdf/Model/Pdf/Items/Totals.php
+++ build/app/code/community/Symmetrics/InvoicePdf/Model/Pdf/Items/Totals.php
@@ -22,23 +22,13 @@
  */
 
 /**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
+ * class to render the total block
  *
  * @category  Symmetrics
  * @package   Symmetrics_InvoicePdf
- * @author    symmetrics gmbh <info@symmetrics.de>
+ * @author    Symmetrics GmbH <info@symmetrics.de>
  * @author    Torsten Walluhn <tw@symmetrics.de>
- * @copyright 2010 Symmetrics Gmbh
+ * @copyright 2010 symmetrics gmbh
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @link      http://www.symmetrics.de/
  */
@@ -46,8 +36,18 @@ class Symmetrics_InvoicePdf_Model_Pdf_Items_Totals
     extends Symmetrics_InvoicePdf_Model_Pdf_Items_Abstract
 {
 
+    /**
+     * default total model
+     *
+     * @var string
+     */
     protected $_defaultTotalModel = 'sales/order_pdf_total_default';
-    
+
+    /**
+     * draw the total block
+     *
+     * @return void
+     */
     public function draw()
     {
         $source = $this->getSource();
@@ -70,8 +70,24 @@ class Symmetrics_InvoicePdf_Model_Pdf_Items_Totals
                 foreach ($total->getTotalsForDisplay() as $totalData) {
                     $tableRowItem = Mage::getModel('invoicepdf/pdf_items_item');
                     /* @var $tableRowItem Symmetrics_InvoicePdf_Model_Pdf_Items_Item */
-                    $tableRowItem->addColumn('label_' . $index, $totalData['label'], 120, 'right', 0, $font, $totalData['font_size']);
-                    $tableRowItem->addColumn('amount_' . $index, $totalData['amount'], 10, 'right', 0, $font, $totalData['font_size']);
+                    $tableRowItem->addColumn(
+                        'label_' . $index,
+                        $totalData['label'],
+                        120, 'right',
+                        0,
+                        $font,
+                        $totalData['font_size']
+                    );
+
+                    $tableRowItem->addColumn(
+                        'amount_' . $index,
+                        $totalData['amount'],
+                        10,
+                        'right',
+                        0,
+                        $font,
+                        $totalData['font_size']
+                    );
 
                     $this->addRow($tableRowItem);
                     $index++;
@@ -83,7 +99,16 @@ class Symmetrics_InvoicePdf_Model_Pdf_Items_Totals
         $this->setPage($page);
     }
 
-    protected function _sortTotalsList($a, $b) {
+    /**
+     * sort the total list by 'sort_order' key
+     *
+     * @param array $a array to sort
+     * @param array $b array to sort
+     * 
+     * @return array
+     */
+    protected function _sortTotalsList($a, $b)
+    {
         if (!isset($a['sort_order']) || !isset($b['sort_order'])) {
             return 0;
         }
@@ -95,6 +120,13 @@ class Symmetrics_InvoicePdf_Model_Pdf_Items_Totals
         return ($a['sort_order'] > $b['sort_order']) ? 1 : -1;
     }
 
+    /**
+     * get the totals list for Source
+     *
+     * @param Mage_Core_Model_Abstract $source soruce to get the totales
+     * 
+     * @return array
+     */
     protected function _getTotalsList($source)
     {
         $totals = Mage::getConfig()->getNode('global/invoicepdf/totals')->asArray();
@@ -107,7 +139,9 @@ class Symmetrics_InvoicePdf_Model_Pdf_Items_Totals
                     $totalInfo['model'] = $totalModel;
                 } else {
                     Mage::throwException(
-                        Mage::helper('sales')->__('Pdf total model should extend Mage_Sales_Model_Order_Pdf_Total_Default')
+                        Mage::helper('sales')->__(
+                            'Pdf total model should extend Mage_Sales_Model_Order_Pdf_Total_Default'
+                        )
                     );
                 }
             } else {
